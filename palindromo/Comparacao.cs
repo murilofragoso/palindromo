09c02bb8765eee9f5a233e440465ccf5d3ed6594
murilofragoso <mu_fragoso@hotmail.com>
2017-07-26 16:21:57 -0300

    desenvolvendo arquivos do palindromo

---
+++ palindromo/Comparacao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace palindromo
+{
+    public class Comparacao
+    {
+        
+    }
+}
---
+++ palindromo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palin;
+            Console.WriteLine("Digite uma palavra");
+            palin = Console.ReadLine();
+            string[] palavra = new string[palin.Length];
+            string[] pa = new string[palin.Length];
+            int y = palin.Length -1;
+            for (var a = 0 ; y >= 0  ; a++,y--)
+            {
+                palavra[y] = palin[a].ToString();
+                pa[a] = palin[a].ToString();
+            }
+            int x = 0;
+            for (int i = 0; i < palin.Length - 1; i++)
+            {
+                if (pa[i] == palavra[i])
+                    x++;
+            }
+            if (x == palin.Length -1)
+            {
+                Console.WriteLine("Sua palavra e um palindromo!");
+            }
+            else
+            {
+                Console.WriteLine("Sua palavra nao e um palindromo!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
